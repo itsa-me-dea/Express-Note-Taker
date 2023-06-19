55163235ac6e03802fd7bcc9690628a923e19c74
itsa-me-dea <wachadea@gmail.com>
2023-06-19 15:55:21 -0700

    set up on Heroku

--- server.js
+++ server.js
@@ -21,6 +21,16 @@ app.get("/notes", (req, res) =>
    res.sendFile(path.join(__dirname, "./public/notes.html"))
 );
 
+// grab notes stored in db.json
+app.get("/api/notes", (req, res) => 
+    fs.readFile(path.join(__dirname, "./db/db.json"), "utf8", (error,notes) => {
+        if (error) {
+            return console.log(error)
+        }
+        res.json(JSON.parse(notes))
+    })
+  );
+
 // listen() method is responsible for listening for incoming connections on the specified port 
 app.listen(PORT, () =>
   console.log(`App listening on http://localhost:${PORT}`)
