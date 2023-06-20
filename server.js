6b693cf91f4d208b3aed7257bc3cf4d9e985e782
itsa-me-dea <wachadea@gmail.com>
2023-06-19 17:00:44 -0700

    added ability to post new notes

--- server.js
+++ server.js
@@ -9,10 +9,13 @@ const app = express();
 // Specify on which port the Express.js server will run
 const PORT = 3001;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 // Static middleware pointing to the public folder
 app.use(express.static('public'));
 
-// Create Express.js routes for default '/', '/send' and '/routes' endpoints
+// Direct route to index html
 app.get("/", (req, res) => 
     res.sendFile(path.join(__dirname, "./public/index.html"))
 );
@@ -31,7 +34,58 @@ app.get("/api/notes", (req, res) =>
     })
   );
 
+// POST request to add a note
+app.post('/api/notes', (req, res) => {
+    // Log that a POST request was received
+    console.info(`${req.method} request received to add a note`);
+  
+    // Destructuring assignment for the items in req.body
+    const { title, text } = req.body;
+  
+    // If all the required properties are present
+    if (title && text) {
+      // Variable for the object to be saved
+      const newNote = {
+        title,
+        text,
+      };
+  
+      // Obtain existing notes
+      fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+        } else {
+          // Convert string into JSON object
+          const parsedNotes = JSON.parse(data);
+  
+          // Add a new note
+          parsedNotes.push(newNote);
+  
+          // Write updated notes back to the file
+          fs.writeFile(
+            './db/db.json',
+            JSON.stringify(parsedNotes, null, 4),
+            (writeErr) =>
+              writeErr
+                ? console.error(writeErr)
+                : console.info('Successfully updated notes!')
+          );
+        }
+      });
+  
+      const response = {
+        status: 'success',
+        body: newNote,
+      };
+  
+      console.log(response);
+      res.status(201).json(response);
+    } else {
+      res.status(500).json('Error in posting note');
+    }
+  });
+
 // listen() method is responsible for listening for incoming connections on the specified port 
 app.listen(PORT, () =>
-  console.log(`App listening on http://localhost:${PORT}`)
+  console.log(`App listening on http://localhost:${PORT} 🚀`)
 );
